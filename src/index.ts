aff0a3cc912b21a75fb869336f08aa816c57af3d
MyFirstRepo <adityasaah932@gmail.com>
2026-03-06 12:36:58 +0530

    Learn : learn about basics and interface and type and generics

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,25 @@
+interface User<T> {
+    name: T;
+    age: number;
+    isStudent: boolean;
+}
+
+const u1: User<string> = {
+    name: "Aditya",
+    age: 22,
+    isStudent: true
+};
+
+console.log(u1.age);
+console.log(u1.name);
+
+
+type tea = "Ginger" | "Lemon";
+
+const t1: tea = "Ginger";
+
+console.log(t1)
+
 const add= (a: number,b: number) : number => {
     return a+b;
 }
